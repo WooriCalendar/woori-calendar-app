0ce369a2d586215e83862fee72afab0f616f1c93
ceojun7 <7ceojun@naver.com>
2023-05-30 16:18:06 +0900

    JDK 1.8로 수정

---
+++ src/main/java/com/ceojun7/wooricalendar/WooricalendarApplication.java
@@ -0,0 +1,13 @@
+package com.ceojun7.wooricalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooricalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooricalendarApplication.class, args);
+    }
+
+}
