5e39cca2e1c32e19b5ea09853e63c654b7edb877
ceojun7 <7ceojun@naver.com>
2023-05-31 20:13:54 +0900

    FullCalendarApp 컴포넌트 생성 및 App.js에 FullCalendarApp.js적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import FullCalendarApp from "./FullCalendarApp";
+import FullCalendarApp from "./components/FullCalendarApp";
 
 function App() {
   return (
---
+++ src/components/FullCalendarApp.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+
+export default class FullCalendarApp extends React.Component {
+    render() {
+        /**
+         * 이벤트 클릭시 발생 함수
+         * @param eventInfo
+         */
+        const handleEventClick = (eventInfo) => {
+            console.log('클릭한 이벤트:', eventInfo.event);
+        };
+
+        /**
+         * @Author 함준혁
+         * 날짜 클릭시 발생 함수
+         * @param arg
+         */
+        const handleDateClick = (arg) => { // bind with an arrow function
+            alert(arg.dateStr)
+        }
+
+        const events = [
+            { title: '이벤트 1', date: '2023-05-31' },
+            { title: '이벤트 2', date: '2023-06-01' },
+            // 추가적인 이벤트 데이터...
+        ];
+        return (
+            /**
+             * FullCalendar Api 추가
+             * 함준혁
+             * 2023-05-31
+             */
+            <FullCalendar
+                plugins={[ dayGridPlugin ]}
+                initialView="dayGridMonth"
+                events={events}
+                eventClick={handleEventClick}
+            />
+        )
+    }
+}
