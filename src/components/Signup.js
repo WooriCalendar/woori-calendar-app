7d8d9a3137e38f657a81c05994892d0ca857349a
ovmkas <ovmkas@gmail.com>
2023-05-31 17:35:37 +0900

    Signup.js 생성

---
+++ src/components/Signup.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const Signup = () => {
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const email = data.get("email");
+        const password = data.get("password");
+        const nickname = data.get("nickname");
+        const subemail = data.get("subemail");
+        const birthday = data.get("birthday");
+        const regDate = data.get("regDate");
+        const updateDate = data.get("updateDate");
+    }
+    return (
+        <Container component="main" maxWidth="xs" style={{ marginTop: "8%" }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography component="h1" variant="h5">
+                        계정 생성
+                    </Typography>
+                </Grid>
+            </Grid>
+            <form noValidate onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="username"
+                            name="username"
+                            label="아이디"
+                            autoComplete="username"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="password"
+                            name="password"
+                            label="패스워드"
+                            type="password"
+                            autoComplete="current-password"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button type="submit" fullWidth variant="contained" color="primary">
+                            계정 생성
+                        </Button>
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item>
+                        <Link to="/login" variant="body2">
+                            이미 계정이 있습니까? 로그인 하세요.
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </Container>
+    );
+};
+
+export default Signup;
