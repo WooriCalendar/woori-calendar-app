49aa4bfb4102dfbb2606e2c3ca27ea86ab6c82d6
ovmkas <ovmkas@gmail.com>
2023-06-08 17:47:53 +0900

    회원가입(브라우저 언어까지 영속화) 완료

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Container, Grid, TextField, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 import {signup} from "../service/ApiService";
+import companyLogo from "../assets/logo(ver3).png";
+import SignupTextField from "./SignupTextField";
 /**
  * 파일명: Signup.js
  * 작성자: 이동건
@@ -9,110 +11,92 @@ import {signup} from "../service/ApiService";
  * 설명: 회원가입 폼을 위한 컴포넌트
  */
 const Signup = () => {
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-        const data = new FormData(event.target);
-        const email = data.get("email");
-        const password = data.get("password");
-        const nickname = data.get("nickname");
-        const subemail = data.get("subemail");
-        const birthday = data.get("birthday");
-        const regDate = data.get("regDate");
-        const updateDate = data.get("updateDate");
-        signup({email, password, nickname,  subemail, birthday, regDate, updateDate}).then(
-            (resp) => (window.location.href = "/login")
-        );
+
+    const [isEmailVisible, setIsEmailVisible] = useState(true);
+    const [isPassVisible, setIsPassVisible] = useState(true);
+    const [isNicknameVisible, setIsNicknameVisible] = useState(true);
+    const [isBirthdayVisible, setIsBirthdayVisible] = useState(true);
+    const [isSubemailVisible, setIsSubemailVisible] = useState(true);
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [nickname, setNickname] = useState();
+    const [subemail, setSubemail] = useState();
+    const [birthday, setBirthday] = useState();
+    const [language, setLanguage] = useState();
+
+    const handleButtonClick = (event) => {
+        if(isEmailVisible || isPassVisible || isNicknameVisible || isBirthdayVisible) {
+            event.preventDefault();
+            setLanguage(window.navigator.language);
+            if (isEmailVisible) {
+                setEmail(document.getElementById('email').value);
+                setIsEmailVisible(false);
+                document.getElementById('email').value = "";
+            } else if (isPassVisible) {
+                setPassword(document.getElementById('password').value);
+                setIsPassVisible(false);
+                document.getElementById('password').value = "";
+            } else if (isNicknameVisible) {
+                setNickname(document.getElementById('nickname').value);
+                setIsNicknameVisible(false);
+                document.getElementById('nickname').value = null;
+            } else if (isSubemailVisible) {
+                setSubemail(document.getElementById('subemail').value);
+                setIsSubemailVisible(false);
+                document.getElementById('subemail').value = null;
+            } else if (isBirthdayVisible) {
+                setBirthday(document.getElementById('birthday').value);
+                setIsBirthdayVisible(false);
+                document.getElementById('birthday').value = null;
+                document.getElementById('handleButton').innerText = "Account Create";
+            }
+        }
+        else {
+            signup({email, password, nickname, subemail, birthday, language}).then(
+                (resp) => (window.location.href = "/login")
+            );
+        }
     }
+
     return (
-        <Container component="main" maxWidth="xs" style={{ marginTop: "8%" }}>
+        <Container component="main" maxWidth="xs" style={{marginTop: "8%"}}>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                    <Typography component="h1" variant="h5">
-                        Account Create
-                    </Typography>
+                    <img src={companyLogo} alt="Woori. logo" style={{width: "100%", marginBottom: "10%"}}/>
                 </Grid>
             </Grid>
-            <form noValidate onSubmit={handleSubmit}>
+            <form noValidate onSubmit={handleButtonClick}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="email"
-                            name="email"
-                            label="email"
-                            autoComplete="email"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="password"
-                            name="password"
-                            label="password"
-                            type="password"
-                            autoComplete="current-password"
-                        />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="nickname"
-                            name="nickname"
-                            label="nickname"
-                            type="nickname"
-                            autoComplete="nickname"
-                        />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="subemail"
-                            name="subemail"
-                            label="subemail"
-                            type="subemail"
-                            autoComplete="subemail"
-                        />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            required
-                            fullWidth
-                            id="birthday"
-                            name="birthday"
-                            label="birthday"
-                            type="birthday"
-                            autoComplete="birthday"
-                        />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <Button type="submit" fullWidth variant="contained" color="primary">
+                        <Typography component="h1" variant="h5">
                             Account Create
-                        </Button>
+                        </Typography>
                     </Grid>
                 </Grid>
-                <Grid container>
-                    <Grid item>
-                        <Link to="/login" variant="body2">
-                            Already have an account? sign in.
-                        </Link>
+                <Grid container spacing={2} style={{marginTop: "5%"}}>
+                    <Grid item xs={12}>
+                        {isEmailVisible ? (
+                            <SignupTextField value="email"/>
+                        ) : isPassVisible ? (
+                            <SignupTextField value="password"/>
+                        ) : isNicknameVisible ? (
+                            <SignupTextField  value="nickname"/>
+                        ) : isSubemailVisible ? (
+                            <SignupTextField  value="subemail"/>
+                        ) : isBirthdayVisible ? (
+                            <SignupTextField value="birthday"/>
+                        ) : ''}
                     </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button id="handleButton" fullWidth variant="contained" color="primary"  type="submit"
+                            onClick={handleButtonClick}
+                            style={{marginTop: "5%"}}>
+                        Next
+                    </Button>
                 </Grid>
             </form>
         </Container>
     );
 };
-
 export default Signup;
