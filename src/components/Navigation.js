f1756d9475c2493405036698e5ee319afb58f90f
ceojun7 <7ceojun@naver.com>
2023-06-09 12:52:44 +0900

    - Navigation 바에 Bar(ICON) 추가 - Navigation 바에 logo-Image 추가 - Navigation 바에 logo-Image 크기 줄이기 - Navigation 바에 icon, 글씨, 상하 중간에 배치하기

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,26 @@
-import { AppBar, Button, Grid, Toolbar, Typography } from "@mui/material";
+import {AppBar, Button, Grid, Toolbar, Typography} from "@mui/material";
 import React from "react";
-import { signout } from "../service/ApiService";
+import {signout} from "../service/ApiService";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import logoImage from '../assets/logo(ver3)small.png';
 
 const Navigation = () => {
+
     return (
-        <AppBar position="static">
+        <AppBar position="static" color="default">
             <Toolbar>
-                <Grid justifyContent="space-between" container>
-                    <Grid item>
+                <Grid container spacing={2} alignItem="center">
+                    <Grid item style={{ marginTop : '10px'}}>
+                        <FontAwesomeIcon icon={faBars} className="bar fa-2x"/>
+                    </Grid>
+                    <Grid item style={{ marginTop : '0px'}}>
+                        <img src={logoImage} alt="Logo Image"/>
+                    </Grid>
+                    <Grid item style={{ marginTop : '10px'}}>
                         <Typography variant="h6">Woori Calendar</Typography>
                     </Grid>
-                    <Grid item>
+                    <Grid item style={{ marginLeft : 'auto', marginTop : '10px'}}>
                         <Button color="inherit" onClick={signout}>
                             Logout
                         </Button>
