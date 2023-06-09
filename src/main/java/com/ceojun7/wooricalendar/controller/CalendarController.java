ddfc19a1cf37772eccf43cc586314920925beeb7
서라 <ksha08@naver.com>
2023-06-09 13:23:30 +0900

    캘린더 생성 시 공유 테이블에 추가

--- src/main/java/com/ceojun7/wooricalendar/controller/CalendarController.java
+++ src/main/java/com/ceojun7/wooricalendar/controller/CalendarController.java
@@ -3,10 +3,15 @@ package com.ceojun7.wooricalendar.controller;
 import com.ceojun7.wooricalendar.dto.CalendarDTO;
 import com.ceojun7.wooricalendar.dto.ResponseDTO;
 import com.ceojun7.wooricalendar.model.CalendarEntity;
+import com.ceojun7.wooricalendar.model.MemberEntity;
+import com.ceojun7.wooricalendar.model.ShareEntity;
+import com.ceojun7.wooricalendar.persistence.ShareRepository;
 import com.ceojun7.wooricalendar.service.CalendarService;
+import com.ceojun7.wooricalendar.service.ShareService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,6 +38,9 @@ public class CalendarController {
     @Autowired
     private CalendarService service;
 
+    @Autowired
+    private ShareService shareService;
+
     /**
      * methodName : createSchedule
      * comment : 새 calendar 생성
@@ -46,13 +54,18 @@ public class CalendarController {
      * @DeleteMapping 생성
      */
     @PostMapping
-    public ResponseEntity<?> createCalendar(@RequestBody CalendarDTO dto) {
+    public ResponseEntity<?> createSchedule(@RequestBody CalendarDTO dto, @AuthenticationPrincipal String email) {
         log.warn(String.valueOf(dto));
         try {
             CalendarEntity entity = CalendarDTO.toEntity(dto);
             List<CalendarEntity> entities = service.create(entity);
             List<CalendarDTO> dtos = entities.stream().map(CalendarDTO::new).collect(Collectors.toList());
             ResponseDTO<CalendarDTO> response = ResponseDTO.<CalendarDTO>builder().data(dtos).build();
+
+            // 캘린더 생성 시 생성된 캘린더 구독
+            ShareEntity shareEntity = ShareEntity.builder().calendarEntity(entity).memberEntity(MemberEntity.builder().email(email).build()).checked(true).build();
+            shareService.create(shareEntity);
+
             return ResponseEntity.ok().body(response);
         } catch (Exception e) {
             e.printStackTrace();
