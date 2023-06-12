2c5f4df778125597d3b9c3aa6f6b64dfeb201a97
ceojun7 <7ceojun@naver.com>
2023-06-12 17:43:59 +0900

    Category.js 추가 기능 : 사용자의 캘린더를 가져올 수 있음.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,9 @@
 import '../css/Sidebar.css';
 import FullCalendars from "./FullCalendars";
+import Category from "./Category";
+import {Button, IconButton, TextField} from "@mui/material";
+import {Search} from "@mui/icons-material";
+import {Container, Grid} from "@mui/material";
 
 const Sidebar = (
     {visible, aspectRatio, height, contentHeight}
@@ -11,6 +15,9 @@ const Sidebar = (
     };
     return (
         <div>
+            <Button variant="contained" color="primary">
+                일정추가버튼
+            </Button>
             {visible && (
                 <div className="slide-out">
                     <FullCalendars
@@ -21,8 +28,15 @@ const Sidebar = (
                     />
                 </div>
             )}
+            <form>
+                <TextField
+                    label="검색"
+                    variant="outlined"
+                    size="small"
+                />
+            </form>
+            <Category/>
         </div>
     );
 };
-
 export default Sidebar;
