20efbf305cfe2995d3172cef8ae2c17382ffe708
Hamdoson <7ceojun@gmail.com>
2023-06-15 21:43:08 +0900

    Navigation 상단에 월/주/일 Select 배치 알림 아이콘 추가 상단에 알림 버튼 추가

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,17 @@
-import {AppBar, Button, Grid, Toolbar, Typography} from "@mui/material";
+import {
+    AppBar,
+    Button,
+    Grid,
+    Toolbar,
+    Typography
+} from "@mui/material";
 import React from "react";
 import {signout} from "../service/ApiService";
 import {faBars} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import logoImage from '../assets/logo(ver3)small.png';
+import SelectLabel from "./SelectLabel";
+import Notification from "./Notification";
 
 const Navigation = (
     {SideBar}
@@ -11,19 +19,25 @@ const Navigation = (
     return (
         <AppBar position="static" color="default">
             <Toolbar>
-                <Grid container spacing={2} alignItem="center">
-                    <Grid item style={{ marginTop : '10px'}}>
+                <Grid container spacing={2} align-item="center">
+                    <Grid item style={{marginTop: '10px'}}>
                         <Button>
-                        <FontAwesomeIcon onClick={SideBar} icon={faBars} className="bar fa-2x"/>
+                            <FontAwesomeIcon onClick={SideBar} icon={faBars} size="2xl" style={{color: "black"}}/>
                         </Button>
                     </Grid>
-                    <Grid item style={{ marginTop : '0px'}}>
+                    <Grid item style={{marginTop: '0px'}}>
                         <img src={logoImage} alt="Logo Image"/>
                     </Grid>
-                    <Grid item style={{ marginTop : '10px'}}>
+                    <Grid item style={{marginTop: '10px'}}>
                         <Typography variant="h6">Woori Calendar</Typography>
                     </Grid>
-                    <Grid item style={{ marginLeft : 'auto', marginTop : '10px'}}>
+                    <Grid item style={{marginLeft: 'auto', marginTop: '10px'}}>
+                        <Notification/>
+                    </Grid>
+                    <Grid item>
+                        <SelectLabel/>
+                    </Grid>
+                    <Grid item style={{marginLeft: '0', marginTop: '10px'}}>
                         <Button color="inherit" onClick={signout}>
                             Logout
                         </Button>
