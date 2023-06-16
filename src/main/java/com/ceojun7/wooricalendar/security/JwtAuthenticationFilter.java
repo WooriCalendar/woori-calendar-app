9a65739a6b49371381bafb375f902582d653a573
ovmkas <ovmkas@gmail.com>
2023-06-16 21:51:48 +0900

    git test

--- src/main/java/com/ceojun7/wooricalendar/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ceojun7/wooricalendar/security/JwtAuthenticationFilter.java
@@ -35,7 +35,7 @@ import lombok.extern.slf4j.Slf4j;
  **/
 @Slf4j
 @Component
-@PropertySource("classpath:../resources/application.properties")
+
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private TokenProvider tokenProvider;
