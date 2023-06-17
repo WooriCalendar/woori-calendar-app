fcd2aef388c48324f4800cee513de1451fa4f2a2
ovmkas <ovmkas@gmail.com>
2023-06-17 19:59:38 +0900

    ForGot Route 추가

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -41,6 +41,7 @@ const AppRouter = () => {
           <Route path="PrivacyPolicyEn" element={<PrivacyPolicyEn />} />
           <Route path="PrivacyPolicyKo" element={<PrivacyPolicyKo />} />
           <Route path="PrivacyPolicyJa" element={<PrivacyPolicyJa />} />
+          <Route path="Forgotpassword" element={<Forgot />}/>
           <Route path={"schedule"} element={<ScheduleCreate/>} />
           <Route path={"calendar"} element={<CalendarCreate/>} />
         </Routes>
