0d9f160f1a2f5927805ab8d66c85e1cf45a5afd6
hyunmin <parkhm323@gmail.com>
2023-06-22 10:45:09 +0900

    메일 발송

--- src/components/ShareModal.js
+++ src/components/ShareModal.js
@@ -11,31 +11,29 @@ import {
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import CloseIcon from "@mui/icons-material/Close";
-import {call, inviteEmail, signupemail} from "../service/ApiService";
+import { call, inviteEmail, signupemail } from "../service/ApiService";
 
 const ShareModal = (props) => {
   const { open, close, calNo, name } = props;
 
-  const [grade, setGrade] = useState("");
+  const [grade, setGrade] = useState(0);
   const [email, setEmail] = useState("");
   const [code, setCode] = useState();
   const [search, setSearch] = useState(false);
-    const [timerId, setTimerId] = useState(null);
   console.log("상위 컴포넌트에서 받아온 캘린더 번호", calNo);
   console.log("상위 컴포넌트에서 받아온 캘린더 이름", name);
   const handleChange = (event) => {
     setGrade(event.target.value);
   };
 
-
   // 캘린더 초대 이벤트
   const invite = () => {
-    // const email = document.getElementById("email").value;
     console.log("메일 : ", email);
     console.log("캘린더 번호 : ", calNo);
     console.log("캘린더 이름 : ", name);
+    console.log("권한 : ", grade);
     setTimeout(() => {
-      inviteEmail({ email, calNo, name }).then((resp) => {
+      inviteEmail({ email, calNo, name, grade }).then((resp) => {
         console.log("발송");
         setCode(resp);
         console.log(code);
@@ -43,43 +41,43 @@ const ShareModal = (props) => {
     }, 100);
   };
 
-
   const handleInputChange = (event) => {
-    setEmail( event.target.value);
+    setEmail(event.target.value);
   };
 
   useEffect(() => {
-
     if (email.includes(".")) {
       setSearch(true);
       console.log(".이 포함되어있는거 감지");
       sendSearchRequest();
-    } else if(email.length < 8 && email.length > 1){
+    } else if (email.length < 8 && email.length > 1) {
       document.getElementById("emailCheck").innerText = "";
-    } {
+    }
+    {
       setSearch(false);
       console.log(".이 안포함되어있음");
     }
 
     // 다른 코드를 여기에 추가할 수 있습니다.
-
   }, [email]); // 의존성 배열에 email 추가
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       // 검색 값 보내기
-      console.log("엔터감지")
+      console.log("엔터감지");
       sendSearchRequest();
     }
   };
 
   const sendSearchRequest = () => {
     // 검색 요청을 처리하는 로직을 구현하고, searchText를 활용합니다.
-    console.log('검색 요청:', email);
-    call("/member/findemail", "POST", {email}).then((resp)=> {
-        if (resp.email){
-          document.getElementById("emailCheck").innerHTML = `<button id="email">${email}</button>`;
-        }
+    console.log("검색 요청:", email);
+    call("/member/findemail", "POST", { email }).then((resp) => {
+      if (resp.email) {
+        document.getElementById(
+          "emailCheck"
+        ).innerHTML = `<button id="email">${email}</button>`;
+      }
     });
   };
 
@@ -107,7 +105,7 @@ const ShareModal = (props) => {
                   endAdornment: (
                     <InputAdornment position="end">
                       <Button onClick={sendSearchRequest}>
-                      <SearchIcon />
+                        <SearchIcon />
                       </Button>
                     </InputAdornment>
                   ),
