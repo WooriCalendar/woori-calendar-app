37262f34f76b71559707bb8481c43b33ca873df3
ovmkas <ovmkas@gmail.com>
2023-06-27 09:45:38 +0900

    로그인 시 Loading화면 구현

--- src/App.js
+++ src/App.js
@@ -43,33 +43,34 @@ function App() {
     return (
         <div className="App">
             {loading ? (
-                <Loading />
+                <Loading/>
             ) : (
                 <>
-            <Navigation SideBar={sideBarButton} initialView={onInitialViewChange}/>
-            {/*<Container maxWidth="xl" style={{margin : '0px 0px'}}>*/}
-            <Grid container spacing={3} style={{margin: '0px 0px', justifyContent: 'space-between', width: 'auto'}}>
-                <Grid item xs={2} style={{paddingLeft: '20px'}}>
-                    <Sidebar
-                        visible={isSideBarVisible}
-                        // height={'200px'}
-                        contentHeight={'410px'}
-                        aspectRatio={'2'}
-                        onCategoryChange={onCategoryChange}
-                    />
-                </Grid>
-                <Grid item xs={10} style={{padding: '10px 30px', marginLeft: 'auto'}}>
-                    <FullCalendars
-                        headerToolbar={headerToolbar}
-                        // height={'800px'}
-                        contentHeight={'800px'}
-                        aspectRatio={'3'}
-                        initialView={initialView}
-                        category={category}
-                    />
-                </Grid>
-            </Grid>
-            {/*</Container>*/}
+                    <Navigation SideBar={sideBarButton} initialView={onInitialViewChange}/>
+                    {/*<Container maxWidth="xl" style={{margin : '0px 0px'}}>*/}
+                    <Grid container spacing={3}
+                          style={{margin: '0px 0px', justifyContent: 'space-between', width: 'auto'}}>
+                        <Grid item xs={2} style={{paddingLeft: '20px'}}>
+                            <Sidebar
+                                visible={isSideBarVisible}
+                                // height={'200px'}
+                                contentHeight={'410px'}
+                                aspectRatio={'2'}
+                                onCategoryChange={onCategoryChange}
+                            />
+                        </Grid>
+                        <Grid item xs={10} style={{padding: '10px 30px', marginLeft: 'auto'}}>
+                            <FullCalendars
+                                headerToolbar={headerToolbar}
+                                // height={'800px'}
+                                contentHeight={'800px'}
+                                aspectRatio={'3'}
+                                initialView={initialView}
+                                category={category}
+                            />
+                        </Grid>
+                    </Grid>
+                    {/*</Container>*/}
                 </>
             )}
         </div>
